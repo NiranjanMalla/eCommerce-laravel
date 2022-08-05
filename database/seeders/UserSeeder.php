d8c5e74fd4283ea7be1b6e117ddf850e5f4da95c
Niranjan Malla <mallaniranjan08@gmail.com>
2022-08-05 22:15:10 +0545

    just login page is done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name'=>'Samjhana Shrestha Malla',
+            'email'=>'samjhanamalla@gmail.com',
+            'password'=>Hash::make('123456')
+        ]);
+
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,19 +3,20 @@
 
 <div class="container custom-login">
     <div class="col-sm-4 col-sm-offset-4">
-        <form action="/action_page.php">
+        <form action="login" method="POST">
             <div class="form-group">
+                @csrf
               <label for="email">Email address:</label>
-              <input type="email" class="form-control" id="email">
+              <input type="email" class="form-control" name="email" id="email">
             </div>
             <div class="form-group">
               <label for="pwd">Password:</label>
-              <input type="password" class="form-control" id="pwd">
+              <input type="password" name="password" class="form-control" id="pwd">
             </div>
             <div class="checkbox">
               <label><input type="checkbox"> Remember me</label>
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-danger">Login</button>
           </form>
     </div>
 </div>
